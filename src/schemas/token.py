f79ca973e740b8987643be463afee3468854bd12
joaquinsantacruz <jsantacruz224324@alumnos.info.unlp.edu.ar>
2026-01-23 15:38:06 -0300

    feat: implement user schemas and repository

--- src/schemas/token.py
+++ src/schemas/token.py
@@ -1,10 +1,8 @@
 from pydantic import BaseModel
 
-# Lo que devuelve el endpoint de login
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
-# Lo que contiene el token decodificado
 class TokenPayload(BaseModel):
     sub: str | None = None

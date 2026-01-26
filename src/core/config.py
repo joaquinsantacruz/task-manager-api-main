0996b364e1cbc4c60291cd2ae823ccfc418ac7a4
joaquinsantacruz <jsantacruz224324@alumnos.info.unlp.edu.ar>
2026-01-26 12:02:54 -0300

    fix: update db connection

--- src/core/config.py
+++ src/core/config.py
@@ -5,22 +5,15 @@ from typing import List
 class Settings(BaseSettings):
     """Application settings"""
 
-    # TODO: Define your configuration settings
-    # Consider:
-    # - Logging configuration
-
     PROJECT_NAME: str = "Task Manager API"
     API_V1_STR: str = "/api/v1"
 
-     # - Database connection URL
     DATABASE_URL: PostgresDsn
 
-    # - JWT secret key and algorithm
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    # - CORS origins
     CORS_ORIGINS: List[str] = []
     
     DEBUG: bool = False
